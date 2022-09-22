ba89a7cbcba8829ea389f6f584b6dda0f64bf2dc
yoonjeonghoom <yoonjeonghoom@gmail.com>
2022-09-22 12:56:31 +0900

    리액트로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,44 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+
+function ItrerateList () {
+  let array = ["추천원두", "원두커피", "콜드블루", "선물세트", "카페용품", "과일청"]
+  let array1 = []
+  for (let i = 0; i < array.length; i++) {
+    array1.push(<li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">{ array[i] }</a></li>)
+  }
+  return array1
+}
+
+function ItrerateList2 () {
+  let array = ["이벤트", "로그인", "회원가입", "고객센터", "(장바구니)", "(검색)", "(리스트)"]
+  let array1 = []
+  for (let i = 0; i < array.length; i++) {
+    array1.push(<li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">{ array[i] }</a></li>)
+  }
+  return array1
+}
 
 function Header () {
   return  <header className='header'>
             <h1 className='header_h1'><a href="#"></a>커피에 미친 남자</h1>
             <div className='header_div1'>
               <ul className='header_div1_ul'>
-                <li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">추천원두</a></li>
-                <li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">원두커피</a></li>
-                <li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">콜드블루</a></li>
-                <li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">선물세트</a></li>
-                <li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">카페용품</a></li>
-                <li className='header_div1_ul_li'><a className='header_div1_ul_li_a' href="#">과일청</a></li>
+                { ItrerateList() }
               </ul>
             </div>
             <div className='header_div2'>
               <ul className='header_div2_ul'>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">이벤트</a></li>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">로그인</a></li>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">회원가입</a></li>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">고객센터</a></li>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">(장바구니)</a></li>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">(검색)</a></li>
-                <li className='header_div2_ul_li'><a className='header_div2_ul_li_a' href="#">(리스트)</a></li>
+                { ItrerateList2() }
               </ul>
             </div>
           </header>
     
 }
 
+function Main () {
+  return  <main>
+            <div className='main_div1_img'></div>
+            <div className='main_div2'>
+              <h2 className='main_div2_h2'>베스트</h2>
+            </div>
+            {Array.from({ length: 3 }).map(() => (
+              <div className='main_div3'>
+                {Array.from({ length: 4 }).map(() => (
+                  <GridExample></GridExample>
+                ))}
+              </div>
+            ))}
+          </main>
+}
+
+function GridExample() {
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 1 }).map((_, idx) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <main>
-        <div className='main_div1_img'></div>
-        <div className='main_div2'>
-          <h2 className='main_div2_h2'>베스트</h2>
-        </div>
-      </main>
+      <Main></Main>
     </div>
   );
 }
